5bcfc995b6d8d4a45b6facb149da2f55b1af5f36
Perry <perryc@lancaster.ac.uk>
2020-01-23 14:19:19 +0000

    Added block_test.js to assets and called it in angular.json

---
+++ src/assets/blockly/custom_blocks/block_test.js
@@ -0,0 +1,33 @@
+[{
+  "type": "block_test",
+  "message0": "Test Select %1 %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "TESTOPTIONS",
+      "options": [
+        [
+          "Option 1",
+          "OPTIONONE"
+        ],
+        [
+          "Option 2",
+          "OPTIONTWO"
+        ],
+        [
+          "Option 3",
+          "OPTIONTHREE"
+        ]
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "NAME",
+      "check": "String"
+    }
+  ],
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+}]
